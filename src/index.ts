577bf9d2136025c552c18ec7cfa624353cfc4092
Jonathan Mukai-Heidt <jonathan.mukai-heidt@smartcontract.com>
2020-05-29 23:36:26 -0400

    Play a truly horrible C note

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello whirled.")
+const audioContext = new AudioContext()
+let oscillator = audioContext.createOscillator()
+oscillator.frequency.setValueAtTime(1635, audioContext.currentTime)
+oscillator.connect(audioContext.destination)
+oscillator.start()
